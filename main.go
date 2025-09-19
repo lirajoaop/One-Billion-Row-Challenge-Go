5b7335bd5fe4661cdbe49137e5c3e9345a5ff5e0
João Pedro Lira <joaopedroababa132@gmail.com>
2025-09-18 21:58:24 -0300

    1brc - v1

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type Measurement struct {
-	Nome  string
 	Min   float64
 	Max   float64
 	Sum   float64
@@ -16,20 +18,61 @@ type Measurement struct {
 }
 
 func main() {
+	start := time.Now()
 	measurements, err := os.Open("measurements.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer measurements.Close()
 
+	dados := make(map[string]Measurement)
+
 	scanner := bufio.NewScanner(measurements)
 	for scanner.Scan() {
 		rawData := scanner.Text()
 		semicolon := strings.Index(rawData, ";")
 		location := rawData[:semicolon]
-		temp := rawData[semicolon+1:]
+		rawTemp := rawData[semicolon+1:]
 
-		fmt.Println(location, temp)
-		return
+		temp, _ := strconv.ParseFloat(rawTemp, 64)
+
+		measurement, ok := dados[location]
+		if !ok {
+			measurement = Measurement{
+				Min:   temp,
+				Max:   temp,
+				Sum:   temp,
+				Count: 1,
+			}
+		} else {
+			measurement.Min = min(measurement.Min, temp)
+			measurement.Max = max(measurement.Max, temp)
+			measurement.Sum += temp
+			measurement.Count++
+		}
+
+		dados[location] = measurement
 	}
+
+	locations := make([]string, 0, len(dados))
+	for name := range dados {
+		locations = append(locations, name)
+	}
+
+	sort.Strings(locations)
+
+	fmt.Printf("{")
+	for _, name := range locations {
+		measurement := dados[name]
+		fmt.Printf(
+			"%s=%.1f/%.1f/%.1f, ",
+			name,
+			measurement.Min,
+			measurement.Sum/float64(measurement.Count),
+			measurement.Max,
+		)
+	}
+	fmt.Printf("}\n")
+
+	fmt.Println(time.Since(start))
 }
